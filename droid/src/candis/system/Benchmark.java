0f40c822f2cf5e7e6cf2785c2603ddcd09eac0a3
Enrico Joerns <e.joerns@tu-bs.de>
2012-11-12 02:07:18 +0100

    [droid] Added concept benchmark code classes Benchmark/FakBenchmark

---
+++ droid/src/candis/system/Benchmark.java
@@ -0,0 +1,11 @@
+package candis.system;
+
+/**
+ * Abstract class that provides standard interface for benchmarks
+ *
+ * @author Enrico Joerns
+ */
+public abstract class Benchmark {
+
+	public abstract void run();
+}
---
+++ droid/src/candis/system/FakBenchmark.java
@@ -0,0 +1,22 @@
+package candis.system;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Enrico Joerns
+ */
+public class FakBenchmark extends Benchmark {
+
+	private static final int MAX_FAK = 10000;
+	
+	@Override
+	public void run() {
+		//-- BigInteger solution.
+		BigInteger n = BigInteger.ONE;
+		for (int i = 1; i <= MAX_FAK; i++) {
+			n = n.multiply(BigInteger.valueOf(i));
+//			System.out.println(i + "! = " + n);
+		}
+	}
+}
