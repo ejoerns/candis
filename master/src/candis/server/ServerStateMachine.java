ffab2dd57e6eee16bd97448f27fb560640ee09fa
Enrico Joerns <e.joerns@tu-bs.de>
2012-11-18 01:58:27 +0100

    [master] basic implementation of server state machine

--- master/src/candis/server/ServerStateMachine.java
+++ master/src/candis/server/ServerStateMachine.java
@@ -1,12 +1,17 @@
 package candis.server;
 
+import candis.common.ByteArray;
 import candis.common.Instruction;
+import candis.common.Message;
+import candis.common.RandomID;
 import candis.common.fsm.ActionHandler;
 import candis.common.fsm.FSM;
 import candis.common.fsm.HandlerID;
 import candis.common.fsm.StateEnum;
 import candis.common.fsm.StateMachineException;
 import candis.common.fsm.Transition;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,7 +22,9 @@ import java.util.logging.Logger;
 public class ServerStateMachine extends FSM {
 
 	private static final String TAG = "ClientStateMachine";
-	private static final Logger logger = Logger.getLogger(TAG);
+	private static final Logger LOGGER = Logger.getLogger(TAG);
+	private final ObjectOutputStream mOutStream;
+	private final DroidManager mDroidManager = DroidManager.getInstance();
 
 	private enum ServerStates implements StateEnum {
 
@@ -30,12 +37,10 @@ public class ServerStateMachine extends FSM {
 
 	public enum ServerTrans implements Transition {
 
-		SOCKET_CONNECTED,
-		//		ACCEPT_CONNECTION,
-		//		REJECT_CONNECTION,
-		//		REQUEST_PROFILE,
-		//		SEND_JOB,
-		JOB_FINISHED;
+		CLIENT_BLACKLISTED,
+		CLIENT_NEW,
+		CLIENT_ACCEPTED,
+		POST_JOB;
 	}
 
 	private enum ClientHandlerID implements HandlerID {
@@ -43,47 +48,97 @@ public class ServerStateMachine extends FSM {
 		MY_ID;
 	}
 
-	ServerStateMachine() {
+	ServerStateMachine(final ObjectOutputStream outstream) {
+		super();
+		this.mOutStream = outstream;
 		init();
 	}
 
-	protected void init() {
-		addState(ServerStates.UNCONNECTED);
-		addState(ServerStates.CHECK);
-		addState(ServerStates.PROFILE_REQUESTED);
-		addState(ServerStates.CONNECTED);
-		addState(ServerStates.JOB_SENT);
+	private void init() {
+		addState(ServerStates.UNCONNECTED)
+						.addTransition(
+						Instruction.REQUEST_CONNECTION,
+						ServerStates.CHECK,
+						new ConnectionRequestedHandler());
+		addState(ServerStates.CHECK)
+						.addTransition(
+						ServerTrans.CLIENT_BLACKLISTED,
+						ServerStates.UNCONNECTED,
+						null)
+						.addTransition(
+						ServerTrans.CLIENT_NEW,
+						ServerStates.PROFILE_REQUESTED,
+						null)
+						.addTransition(
+						ServerTrans.CLIENT_ACCEPTED,
+						ServerStates.CONNECTED,
+						null);
+		addState(ServerStates.PROFILE_REQUESTED)
+						.addTransition(
+						Instruction.SEND_PROFILE,
+						ServerStates.CONNECTED,
+						new ReceivedProfileHandler());
+		addState(ServerStates.CONNECTED)
+						.addTransition(
+						ServerTrans.POST_JOB,
+						ServerStates.JOB_SENT,
+						null);
+		addState(ServerStates.JOB_SENT)
+						.addTransition(
+						Instruction.SEND_RESULT,
+						ServerStates.CONNECTED,
+						null);
+		setState(ServerStates.UNCONNECTED);
+	}
 
+	private class ConnectionRequestedHandler implements ActionHandler {
 
-
-		// Run test
-		try {
-			setState(ServerStates.UNCONNECTED);
-			System.out.println("State: " + getState());
-			process(ServerTrans.SOCKET_CONNECTED);
-			System.out.println("State: " + getState());
-			process(Instruction.ACCEPT_CONNECTION);
-			System.out.println("State: " + getState());
-			process(Instruction.SEND_JOB);
-			System.out.println("State: " + getState());
-		} catch (StateMachineException ex) {
-			logger.log(Level.SEVERE, null, ex);
+		@Override
+		public void handle(Object obj) {
+			System.out.println("ConnectionRequestedHandler called");
+			if (obj == null) {
+				LOGGER.log(Level.WARNING, "Missing payload data (expected RandomID)");
+				return;
+			}
+			ByteArray barray = new ByteArray(((RandomID) obj).getBytes());
+			Transition trans;
+			Instruction instr;
+			// check droid in db
+			if (mDroidManager.isDroidKnown(barray)) {
+				if (mDroidManager.isDroidBlacklisted(barray)) {
+					trans = ServerTrans.CLIENT_BLACKLISTED;
+					instr = Instruction.REJECT_CONNECTION;
+				} else {
+					trans = ServerTrans.CLIENT_ACCEPTED;
+					instr = Instruction.ACCEPT_CONNECTION;
+				}
+			} else {
+				trans = ServerTrans.CLIENT_NEW;
+				instr = Instruction.REQUEST_PROFILE;
+			}
+			try {
+				mOutStream.writeObject(new Message(instr));
+				process(trans);
+			} catch (StateMachineException ex) {
+				Logger.getLogger(ServerStateMachine.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (IOException ex) {
+				LOGGER.log(Level.SEVERE, null, ex);
+			}
 		}
 	}
 
-	private class SocketConnectedHandler implements ActionHandler {
+	// TODO: make static?
+	private class ReceivedProfileHandler implements ActionHandler {
 
 		@Override
-		public void handle() {
-			System.out.println("Handle :)");
-		}
-	}
-
-	private class MySecondHandler implements ActionHandler {
-
-		@Override
-		public void handle() {
-			System.out.println("Handle :)");
+		public void handle(Object obj) {
+			System.out.println("ReceivedProfileHandler called");
+			try {
+				// store profile data
+				mOutStream.writeObject(new Message(Instruction.ACCEPT_CONNECTION));
+			} catch (IOException ex) {
+				LOGGER.log(Level.SEVERE, null, ex);
+			}
 		}
 	}
 }
