cf6efec772c7a52744d40191bf96b9d6a73ea8b9
Enrico Joerns <e.joerns@tu-bs.de>
2012-11-21 00:35:14 +0100

    [master] implemented listener interface and event class for registering callbacks at DroidManager

---
+++ master/src/candis/server/DroidManagerListener.java
@@ -0,0 +1,23 @@
+package candis.server;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ *
+ * @author Enrico Joerns
+ */
+public interface DroidManagerListener {
+
+	/**
+	 * Called by DroidManager if Droid list updates.
+	 * 
+	 * @param event Indicates type of Event
+	 * @param knownDroids List of known Droids
+	 * @param connectedDroids List of connected Droids
+	 */
+	void handle(
+					DroidManagerEvent event,
+					Map<String, DroidData> knownDroids,
+					Map<String, AtomicBoolean> connectedDroids);
+}
