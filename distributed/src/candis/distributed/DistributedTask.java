d2851aa3efd79bdd37eab3340b4b218dd0345a43
Sebastian Willenborg <seb.willenborg@googlemail.com>
2012-12-09 14:31:52 +0100

    [distributed] Added Interface to transfer different inital Parameter

--- distributed/src/candis/distributed/DistributedTask.java
+++ distributed/src/candis/distributed/DistributedTask.java
@@ -6,6 +6,8 @@ package candis.distributed;
  */
 public abstract class DistributedTask {
 
+	abstract public void setInitialParameter(DistributedParameter parameter);
+
 	abstract public DistributedResult run(DistributedParameter parameter);
 
 	abstract public void stop();
