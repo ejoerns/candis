319071d55b9fe644374a291fd4070247716525ef
Enrico Joerns <e.joerns@tu-bs.de>
2013-01-16 14:59:17 +0100

    [distributed] tried to make implementing custom Scheduler easier

--- distributed/src/candis/distributed/Scheduler.java
+++ distributed/src/candis/distributed/Scheduler.java
@@ -1,61 +1,262 @@
 package candis.distributed;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Sebastian Willenborg
  */
 public abstract class Scheduler {
-	protected final List<ResultReceiver> mReceivers = new LinkedList<ResultReceiver>();
 
-	public void addResultReceiver(ResultReceiver receiver) {
-		mReceivers.add(receiver);
-	}
+  private static final Logger LOGGER = Logger.getLogger(Scheduler.class.getName());
+  // List of callback receivers
+  protected final List<ResultReceiver> mReceivers = new LinkedList<ResultReceiver>();
+  protected final Stack<DistributedJobParameter> mParams = new Stack<DistributedJobParameter>();
+  protected DistributedJobParameter mInitalParameter = null;
+  protected JobDistributionIO mJobDistIO;
+  /// Results
+  private final Map<DistributedJobParameter, DistributedJobResult> mResults = new HashMap<DistributedJobParameter, DistributedJobResult>();
+  /// Droids that are currently running
+  protected final Map<String, DistributedJobParameter> mRunningDroidsList = new HashMap<String, DistributedJobParameter>();
+  /// Droids that can be scheduled
+  protected final Map<String, DroidData> mSchedulabeDroids = new HashMap<String, DroidData>();
+  private boolean mEnabled = false;
 
-	protected void releaseResult(DistributedJobParameter param, DistributedJobResult result) {
-		for(ResultReceiver receiver: mReceivers) {
-			receiver.onReceiveResult(param, result);
-		}
-	}
+  /**
+   * Sets the JobDistributionIO.
+   * Obligatoric function
+   *
+   * @param io
+   */
+  public void setCommunicationIO(JobDistributionIO io) {
+    mJobDistIO = io;
+  }
 
-	public abstract void setInitialParameter(DistributedJobParameter param);
+  /**
+   * Adds a listener for Results.
+   *
+   * @param receiver
+   */
+  public void addResultReceiver(ResultReceiver receiver) {
+    mReceivers.add(receiver);
+  }
 
-	public abstract void addParameter(DistributedJobParameter param);
+  /**
+   * Called by Scheduler to publish results.
+   *
+   * @param param
+   * @param result
+   */
+  protected void releaseResult(DistributedJobParameter param, DistributedJobResult result) {
+    for (ResultReceiver receiver : mReceivers) {
+      receiver.onReceiveResult(param, result);
+    }
+  }
 
-	public abstract void addParameters(DistributedJobParameter[] params);
+  public void setInitialParameter(DistributedJobParameter param) {
+    mInitalParameter = param;
+  }
 
-	public abstract void setCommunicationIO(JobDistributionIO io);
+  public void addParameter(DistributedJobParameter param) {
+    mParams.push(param);
+  }
 
-	public abstract void start();
+  public void addParameters(DistributedJobParameter[] params) {
+    for (DistributedJobParameter p : params) {
+      this.mParams.push(p);
+    }
+  }
 
-	public abstract void abort();
+  /**
+   * Implement this method to create your own nice and fancy Scheduler.
+   *
+   * @param droidList List of all schedulable Droids.
+   */
+  protected abstract void schedule(Map<String, DroidData> droidList);
 
-	public abstract void onNewDroid(String droidID);
+  public void start() {
+    System.out.println("SS: start()");
+    if (mEnabled) {
+      return;
+    }
+    mEnabled = true;
+    // start scheduler
+    new Thread(new Runnable() {
+      public void run() {
+        System.out.println("SS: XXXXXXXXXXXXXXXXXXXXXXXXX new thread");
+        while (mEnabled) {
+          synchronized (mSchedulabeDroids) {
+            if (hasParameter()) {
+              schedule(mSchedulabeDroids);
+            }
+            try {
+              // todo: seb mach ma!!!!
+              // Weiß er was er hier machen würde?
+              mSchedulabeDroids.wait();
+            }
+            catch (InterruptedException ex) {
+              LOGGER.log(Level.SEVERE, null, ex);
+            }
+          }
+        }
+      }
+    }).start();
+  }
 
-	/**
-	 * Is called when a job is done.
-	 *
-	 * @param droidID
-	 * @param result
-	 */
-	public abstract void onJobDone(String droidID, DistributedJobResult result);
+  /**
+   * Used to do inital checks if the droid can be used for scheduling.
+   *
+   * @return true if accepted, false otherwise
+   */
+  protected boolean checkAccepted() {
+    return true;
+  }
 
-	public abstract void onBinaryRecieved(String droidID);
+  /**
+   * Called when a new droid connected.
+   *
+   * @param droidID
+   */
+  public void onNewDroid(String droidID) {
+    System.out.println("SS: onNewDroid()");
+    // Test if droid is acceptable according to our scheduling rules.
+    if (!checkAccepted()) {
+      LOGGER.log(Level.INFO, "Rejected Droid {0}", droidID);
+      return;
+    }
+    LOGGER.log(Level.INFO, "Got new Droid {0}", droidID);
+    // Send Binary
+    mJobDistIO.sendBinary(droidID);
+  }
 
-	public abstract void onInitParameterRecieved(String droidID);
+  /**
+   * Called when droid received binary.
+   *
+   * @param droidID
+   */
+  public void onBinaryRecieved(String droidID) {
+    System.out.println("SS: onBinaryRecieved()");
+    // Send inital Parameter
+    mJobDistIO.sendInitialParameter(droidID, mInitalParameter);
+  }
 
-	/**
-	 * Is called when an error occurred during computation.
-	 *
-	 * @param droidID
-	 * @param error
-	 */
-	public abstract void onDroidError(String droidID, DistributedJobError error);
+  public void onInitParameterRecieved(String droidID) {
+    System.out.println("SS: onInitParameterRecieved()");
+    // add to list of schedulable tasks
+    synchronized (mSchedulabeDroids) {
+      mSchedulabeDroids.put(droidID, mJobDistIO.getDroidData(droidID));
+      mSchedulabeDroids.notifyAll();
+    }
+  }
 
-	public abstract boolean isDone();
+  /**
+   * Is called when a job is done.
+   *
+   * @param droidID
+   * @param result
+   */
+  // Job is done and new job can be assigned
+  public void onJobDone(String droidID, DistributedJobResult result) {
+    System.out.println("SS: onJobDone()");
+    if (mRunningDroidsList.containsKey(droidID)) {
+      DistributedJobParameter p = mRunningDroidsList.remove(droidID);
+      mResults.put(p, result);
+      releaseResult(p, result);
+      LOGGER.log(Level.INFO, "Param {0} on {1} done with {2}", new Object[]{p, droidID, result});
+    }
+    synchronized (mSchedulabeDroids) {
+      mSchedulabeDroids.put(droidID, mJobDistIO.getDroidData(droidID));
+      mSchedulabeDroids.notifyAll();
+    }
+  }
 
-	public abstract Map<DistributedJobParameter, DistributedJobResult> getResults();
+  /**
+   * Is called when an error occurred during computation.
+   *
+   * @param droidID
+   * @param error
+   */
+  // ups..
+  public void onDroidError(String id, DistributedJobError error) {
+    LOGGER.log(Level.SEVERE, "Droid {0}, Error {1}", new Object[]{id, error});
+    // removed Droids won't stay in the id List
+    // therefore it is not neccesary to check, for DROID_LOST in error
+    if (mRunningDroidsList.containsKey(id)) {
+      DistributedJobParameter p = mRunningDroidsList.get(id);
+      addParameter(p);
+    }
+  }
+
+  public boolean isDone() {
+//    LOGGER.log(Level.FINE,
+//               "running {0}, params {1}, available Droids {2}",
+//               new Object[]{
+//              mRunningDroidsList.size(),
+//              mParams.size(),
+//              mJobDistIO.getDroidCount()});
+//    return (mRunningDroidsList.size() + mParams.size()) == 0;
+    return false;
+  }
+
+  public void abort() {
+    for (String droidID : mSchedulabeDroids.keySet()) {
+      if (mRunningDroidsList.containsKey(droidID)) {
+        mRunningDroidsList.remove(droidID);
+        mJobDistIO.stopJob(droidID);
+      }
+    }
+    mEnabled = false;
+  }
+
+  public Map<DistributedJobParameter, DistributedJobResult> getResults() {
+    return mResults;
+  }
+
+  /**
+   * Tests if parameter stack is empty
+   *
+   * @return
+   */
+  protected boolean hasParameter() {
+    return !mParams.empty();
+  }
+
+  /**
+   * Pops new parameter from the parameter list
+   *
+   * @return Parameter if available, null if no parameter found
+   */
+  protected DistributedJobParameter popParameters() {
+    if (mParams.empty()) {
+      return null;
+    }
+    return mParams.pop();
+  }
+
+  /**
+   * Entry for process table.
+   */
+  protected class ScheduleDroid {
+
+    public ScheduleDroid(String id, DroidData data) {
+      this.id = id;
+      this.running = false;
+      this.currentParameter = null;
+      this.data = data;
+    }
+    /// id of the Droid
+    public String id;
+    /// true if Droid is running a job
+    public boolean running;
+    /// if Droid is runnning: currently assigned task
+    public DistributedJobParameter currentParameter;
+    /// DroidData of Droid
+    public DroidData data;
+  }
 }
--- distributed/src/candis/distributed/SimpleScheduler.java
+++ distributed/src/candis/distributed/SimpleScheduler.java
@@ -1,8 +1,7 @@
 package candis.distributed;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,147 +9,27 @@ import java.util.logging.Logger;
  * Simple Scheduler to assign tasks to Droids without special predictions.
  *
  * Every Droid will get DistributedParameters, if a Droid finishes it's task or
- * a new Droid appears it will get is next DistributedParameter
+ * a new Droid appears it will get its next DistributedParameter
  *
  * @author Sebastian Willenborg
+ * @author Enrico Joerns
  */
 public class SimpleScheduler extends Scheduler {
 
+  private static final Logger LOGGER = Logger.getLogger(SimpleScheduler.class.getName());
 
-	private enum DroidState {
-		NEW,
-		BINARY,
-		INITIAL;
-	}
-
-	private static final Logger LOGGER = Logger.getLogger(SimpleScheduler.class.getName());
-	private JobDistributionIO comIO;
-	private DistributedJobParameter initalParameter = null;
-	private Stack<DistributedJobParameter> params = new Stack<DistributedJobParameter>();
-	private final Map<String, DistributedJobParameter> running = new HashMap<String, DistributedJobParameter>();
-	private final Map<DistributedJobParameter, DistributedJobResult> done = new HashMap<DistributedJobParameter, DistributedJobResult>();
-	private final Map<String, DroidState> droidStates = new HashMap<String, DroidState>();
-	private boolean started = false;
-
-	public SimpleScheduler() {
-	}
-
-	public void setCommunicationIO(JobDistributionIO io) {
-		comIO = io;
-	}
-
-	@Override
-	public Map<DistributedJobParameter, DistributedJobResult> getResults() {
-		return done;
-	}
-	
-	public void start() {
-		started = true;
-		assignTasks();
-	}
-
-	private void assignTasks() {
-		LOGGER.log(Level.INFO, "Assigning to {0} possible Droids", comIO.getDroidCount());
-		for (String droidID : comIO.getConnectedDroids()) {
-			assignTask(droidID);
-		}
-	}
-
-	private boolean assignTask(String droidID) {
-		if (params.isEmpty()) {
-			return false;
-		}
-		if (running.containsKey(droidID)) {
-			return false;
-		}
-		DroidState state = DroidState.NEW;
-		if(droidStates.containsKey(droidID)) {
-			state = droidStates.get(droidID);
-		}
-		switch(state) {
-			case NEW:
-				comIO.sendBinary(droidID);
-				break;
-			case BINARY:
-				comIO.sendInitialParameter(droidID, initalParameter);
-				break;
-			case INITIAL:
-				DistributedJobParameter param = params.pop();
-				comIO.startJob(droidID, param);
-				running.put(droidID, param);
-				break;
-			default:
-				throw new AssertionError(state.name());
-		}
-		return true;
-	}
-
-	public void abort() {
-		for (String droidID : running.keySet()) {
-			comIO.stopJob(droidID);
-		}
-	}
-
-	public void onNewDroid(String droidID) {
-		if (!started) {
-			return;
-		}
-		LOGGER.log(Level.INFO, "Got new Droid {0}", droidID);
-		droidStates.put(droidID, DroidState.NEW);
-		assignTask(droidID);
-	}
-
-	public void onJobDone(String droidID, DistributedJobResult result) {
-		if (running.containsKey(droidID)) {
-			DistributedJobParameter p = running.remove(droidID);
-			done.put(p, result);
-			releaseResult(p, result);
-			LOGGER.log(Level.INFO, "Param {0} on {1} done with {2}", new Object[]{p, droidID, result});
-		}
-		assignTask(droidID);
-	}
-
-	public void onDroidError(String id, DistributedJobError error) {
-		LOGGER.log(Level.SEVERE, "Droid {0}, Error {1}", new Object[]{id, error});
-
-		// removed Droids won't stay in the id List
-		// therefore it is not neccesary to check, for DROID_LOST in error
-		if (running.containsKey(id)) {
-			DistributedJobParameter p = running.get(id);
-			params.push(p);
-		}
-		assignTasks();
-	}
-
-	public void addParameter(DistributedJobParameter param) {
-		params.push(param);
-	}
-
-	public void addParameters(DistributedJobParameter[] params) {
-		for (DistributedJobParameter p : params) {
-			this.params.push(p);
-		}
-	}
-
-	public boolean isDone() {
-		LOGGER.log(Level.FINE, "running {0}, params {1}, available Droids {2}", new Object[]{running.size(), params.size(), comIO.getDroidCount()});
-		return (running.size() + params.size()) == 0;
-	}
-
-	public void setInitialParameter(DistributedJobParameter param) {
-		initalParameter = param;
-	}
-
-	public void onBinaryRecieved(String droidID) {
-		droidStates.put(droidID, DroidState.BINARY);
-		// Send inital Parameter
-		assignTask(droidID);
-
-	}
-
-	public void onInitParameterRecieved(String droidID) {
-		droidStates.put(droidID, DroidState.INITIAL);
-		// Send Job
-		assignTask(droidID);
-	}
+  public void schedule(final Map<String, DroidData> droidList) {
+    // do magic stuff here...
+    // simply start all available jobs on all available droids
+    Iterator<Map.Entry<String, DroidData>> it = droidList.entrySet().iterator();
+    while (it.hasNext()) {
+      Map.Entry<String, DroidData> droid = it.next();
+      DroidData data = droid.getValue();
+      String id = droid.getKey();
+      // TODO: flag setzen
+      it.remove();
+      DistributedJobParameter param = popParameters();
+      mJobDistIO.startJob(id, param);
+    }
+  }
 }
