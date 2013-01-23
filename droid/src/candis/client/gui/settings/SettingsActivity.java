87fb5d71bb83a358918ac382e100bab3dea08dd5
Enrico Joerns <e.joerns@tu-bs.de>
2013-01-23 02:42:43 +0100

    [droid] SettingsActivity: set summary text with corresponding preference values

--- droid/src/candis/client/gui/settings/SettingsActivity.java
+++ droid/src/candis/client/gui/settings/SettingsActivity.java
@@ -1,14 +1,31 @@
 package candis.client.gui.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
+import android.widget.Toast;
 import candis.client.R;
 
 /**
  *
  * @author Enrico Joerns
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+  // Keys as defined in xml/preferences.xml
+  public static final String HOSTNAME = "pref_host";
+  public static final String PORTNAME = "pref_port";
+  public static final String POWERMODE = "pref_powermode";
+  // 
+  private EditTextPreference mHostnamePreference;
+  private EditTextPreference mPortnamePreference;
+  private ListPreference mPowermodePreference;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +33,40 @@ public class SettingsActivity extends PreferenceActivity {
 
     // Load the preferences from an XML resource
     addPreferencesFromResource(R.xml.preferences);
+
+    // 
+    mHostnamePreference = (EditTextPreference) getPreferenceScreen().findPreference(HOSTNAME);
+    mPortnamePreference = (EditTextPreference) getPreferenceScreen().findPreference(PORTNAME);
+    mPowermodePreference = (ListPreference) getPreferenceScreen().findPreference(POWERMODE);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    getPreferenceScreen().getSharedPreferences()
+            .registerOnSharedPreferenceChangeListener(this);
+
+    SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+    mHostnamePreference.setSummary(sharedPreferences.getString(HOSTNAME, ""));
+    mPortnamePreference.setSummary(sharedPreferences.getString(PORTNAME, ""));
+    mPowermodePreference.setSummary(sharedPreferences.getString(POWERMODE, ""));
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    getPreferenceScreen().getSharedPreferences()
+            .unregisterOnSharedPreferenceChangeListener(this);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    Log.i("SettingsActivity", "onSharedPreferenceChanged");
+    // Let's do something a preference value changes
+    if (key.equals(HOSTNAME)) {
+      Preference hostPref = findPreference(key);
+      hostPref.setSummary(sharedPreferences.getString(key, ""));
+    }
   }
 }
