6263a7435165ce58313b4556b359a6c4f91fde1e
Sebastian Willenborg <seb.willenborg@googlemail.com>
2013-01-29 21:44:02 +0100

    [hash] Optimized distributed Brute-Force-Algorithm

--- examples/hash/hashControl/src/candis/example/hash/HashControl.java
+++ examples/hash/hashControl/src/candis/example/hash/HashControl.java
@@ -20,39 +20,44 @@ import candis.example.hash.HashInitParameter.HashType;
  * @author Sebastian Willenborg
  */
 public class HashControl implements DistributedControl {
+
 	private BruteForceScheduler mScheduler;
 
 	@Override
 	public Scheduler initScheduler() {
 		UserParameterSet parameters = new UserParameterSet();
 		// 900150983cd24fb0d6963f7d28e17f72 = md5("abc");
+		// e99a18c428cb38d5f260853678922e03 = md5("abc123")
 		StringUserParameter hashvalue = new StringUserParameter("hash.hashvalue", "Hash (hex)", "Enter hash to crack here",
-						"900150983cd24fb0d6963f7d28e17f72", new HashInputValidator());
+																														"e99a18c428cb38d5f260853678922e03", new HashInputValidator());
 		parameters.AddParameter(hashvalue);
 
 		StringListUserParameter type = new StringListUserParameter("hash.type", "Hash-Method", "Specifiy the type of the hash",
-						0, new String[]{"md5", "sha1"});
+																															 0, new String[]{"md5", "sha1"});
 		parameters.AddParameter(type);
 
 		StringListUserParameter tryAlpha = new StringListUserParameter("hash.try.alpha", "Characters", "Use small charactes (a-z), caps (A-Z) or both.",
-						2, new String[]{"small", "caps", "both", "none"});
+																																	 2, new String[]{"small", "caps", "both", "none"});
 		parameters.AddParameter(tryAlpha);
 
 		BooleanUserParameter tryNumeric = new BooleanUserParameter("hash.try.numeric", "Numbers", "Use Numbers",
-						false);
+																															 false);
 		parameters.AddParameter(tryNumeric);
 
 		StringUserParameter tryElse = new StringUserParameter("hash.try.else", "Other Chars", "Enter other Characters to try",
-						"!@#", new RegexValidator("[^a-zA-Z0-9]*"));
+																													"!@#", new RegexValidator("[^a-zA-Z0-9]*"));
 		parameters.AddParameter(tryElse);
 
 		IntegerUserParameter start = new IntegerUserParameter("hash.trylen.start", "Minimal Length", "Specify the minimal length of the brutefoce string",
-						2, 1, Integer.MAX_VALUE, 1, new SmallerThanValidator("hash.trylen.stop"));
+																													3, 1, Integer.MAX_VALUE, 1, new SmallerThanValidator("hash.trylen.stop"));
 		parameters.AddParameter(start);
 
 		IntegerUserParameter stop = new IntegerUserParameter("hash.trylen.stop", "Maximal Length", "Specify the maximal length of the bruteforce string",
-						3, 1, Integer.MAX_VALUE, 1, new BiggerThanValidator("hash.trylen.start"));
+																												 6, 1, Integer.MAX_VALUE, 1, new BiggerThanValidator("hash.trylen.start"));
 		parameters.AddParameter(stop);
+
+		IntegerUserParameter depth = new IntegerUserParameter("hash.depth", 2, 1,10,1, null);
+		parameters.AddParameter(depth);
 
 		UserParameterRequester.getInstance().request(parameters);
 
@@ -63,39 +68,40 @@ public class HashControl implements DistributedControl {
 		System.out.println("try.else " + tryElse.getValue());
 		System.out.println("trylen.start " + start.getIntegerValue());
 		System.out.println("trylen.stop " + stop.getIntegerValue());
+		System.out.println("depth " + depth.getIntegerValue());
 		//parameters
 
 		String alpha = tryAlpha.getValue().toString();
 		String total = "";
-		if(tryNumeric.getBooleanValue()) {
+		if (tryNumeric.getBooleanValue()) {
 			total += "0123456789";
 		}
-		if(alpha.equals("both") || alpha.equals("caps")) {
-			total += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		}
-		if(alpha.equals("both") || alpha.equals("small")) {
+		if (alpha.equals("both") || alpha.equals("small")) {
 			total += "abcdefghijklmnopqrstuvwxyz";
 		}
+		if (alpha.equals("both") || alpha.equals("caps")) {
+			total += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		}
 
-		for(char c: tryElse.getStringValue().toCharArray()) {
+		for (char c : tryElse.getStringValue().toCharArray()) {
 			// Prevent duplicates
-			if(!total.contains(Character.toString(c))) {
+			if (!total.contains(Character.toString(c))) {
 				total += c;
 			}
 		}
 
 		// Generate BruteForceScheduler
 		HashType typeValue = HashType.MD5;
-		if (type.getValue().toString().equals("sha1")){
+		if (type.getValue().toString().equals("sha1")) {
 			typeValue = HashType.SHA1;
 		}
-		mScheduler = new BruteForceScheduler(start.getIntegerValue(), total.toCharArray(), stop.getIntegerValue(), typeValue, hashvalue.getStringValue());
+		mScheduler = new BruteForceScheduler(start.getIntegerValue(), total.toCharArray(), stop.getIntegerValue(), depth.getIntegerValue(), typeValue, hashvalue.getStringValue());
 		return mScheduler;
 	}
 
 	@Override
 	public void onSchedulerDone() {
-		if(mScheduler.resultValue != null) {
+		if (mScheduler.resultValue != null) {
 			System.out.println(mScheduler.resultValue);
 		}
 		else {
@@ -103,5 +109,4 @@ public class HashControl implements DistributedControl {
 		}
 
 	}
-
 }
