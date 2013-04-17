ea4ea7b8ddbad56feaae33e71c46b5b317e4f07e
Enrico Joerns <e.joerns@tu-bs.de>
2013-04-17 15:21:13 +0200

    miniControl: adapted to new interface

--- examples/mini/miniControl/src/candis/example/mini/MiniControl.java
+++ examples/mini/miniControl/src/candis/example/mini/MiniControl.java
@@ -6,46 +6,65 @@ import candis.distributed.DistributedJobResult;
 import candis.distributed.ResultReceiver;
 import candis.distributed.Scheduler;
 import candis.distributed.SimpleScheduler;
+import java.util.Stack;
 
 /**
  * Minimalistic example how to initialize a set of tasks and receive its
  * results.
  */
-public class MiniControl implements DistributedControl, ResultReceiver {
-
-	MiniInitParameter init;
-	Scheduler scheduler;
-
+public class MiniControl extends DistributedControl implements ResultReceiver {
+	
+	private static final int JOBS = 10;
+	private Scheduler mScheduler;
+	private Stack<DistributedJobParameter> mJobs;
+	
 	@Override
-	public Scheduler initScheduler() {
-		scheduler = new SimpleScheduler();
+	public final Scheduler initScheduler() {
+		mScheduler = new SimpleScheduler(this);
 
 		// Register ResultReceiver
-		scheduler.addResultReceiver(this);
+		mScheduler.addResultReceiver(this);
 
 		// Set initial Parameters
-		init = new MiniInitParameter(23);
-		scheduler.setInitialParameter(init);
+		mScheduler.setInitialParameter(new MiniInitParameter(23));
 
 		// Create some tasks
-		for (int i = 0; i < 10; i++) {
-			scheduler.addParameter(new MiniJobParameter(i, 3.5f));
+		mJobs = new Stack<DistributedJobParameter>();
+		for (int i = 0; i < JOBS; i++) {
+			mJobs.add(new MiniJobParameter(i, 3.5f));
 		}
-
-		return scheduler;
+		
+		return mScheduler;
 	}
-
+	
 	@Override
-	public void onSchedulerDone() {
+	public final void onSchedulerDone() {
 		// Now all tasks are completed successfully
 		System.out.println("done!");
 	}
-
+	
 	@Override
-	public void onReceiveResult(DistributedJobParameter param, DistributedJobResult result) {
+	public final void onReceiveResult(DistributedJobParameter param, DistributedJobResult result) {
 		/// One result is finished and we can use it, somehow ...
 		MiniJobResult miniResult = (MiniJobResult) result;
 		System.out.println(String.format("Got Result: %.3f", miniResult.foobar));
-
+	}
+	
+	@Override
+	public final DistributedJobParameter getParameter() {
+		System.out.println("getParameter " + mJobs.pop());
+		return mJobs.pop();
+	}
+	
+	@Override
+	public final long getParametersLeft() {
+		System.out.println("getParametersLeft " + mJobs.size());
+		return mJobs.size();
+	}
+	
+	@Override
+	public final boolean hasParametersLeft() {
+		System.out.println("hasParametersLeft " + mJobs.empty());
+		return !mJobs.empty();
 	}
 }
