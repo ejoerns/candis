89e0b6fa2bbf390f7f24a7fc06cd8e3899482343
Enrico Joerns <e.joerns@tu-bs.de>
2013-04-18 23:05:10 +0200

    AnalyzerScheduler: improved output

--- distributed/src/candis/distributed/AnalyzerScheduler.java
+++ distributed/src/candis/distributed/AnalyzerScheduler.java
@@ -11,9 +11,11 @@ import java.util.Map;
  *
  * @author Enrico Joerns
  */
-public class AnalyzerScheduler extends SimpleScheduler {
+public class AnalyzerScheduler extends ProfilingScheduler {
 
   private Map<String, List<Long>> mExecutionTimes = new HashMap<String, List<Long>>();
+  private Map<String, Integer> mProcessedParameters = new HashMap<String, Integer>();
+  private List<TestData> mData = null;
   long start, fulltime;
 
   public AnalyzerScheduler(DistributedControl control) {
@@ -37,15 +39,19 @@ public class AnalyzerScheduler extends SimpleScheduler {
     super.onJobDone(droidID, jobID, results, exectime);
     if (!mExecutionTimes.containsKey(droidID)) {
       mExecutionTimes.put(droidID, new LinkedList<Long>());
+      mProcessedParameters.put(droidID, 0);
     }
     mExecutionTimes.get(droidID).add(exectime);
+    int pcnt = mProcessedParameters.remove(droidID) + results.length;
+    mProcessedParameters.put(droidID, pcnt);
   }
 
   @Override
   public boolean isDone() {
     boolean done = super.isDone();
 
-    if (done) {
+    if (done && (mData == null)) {
+      mData = new LinkedList<TestData>();
       fulltime = System.currentTimeMillis() - start;
 
       try {
@@ -54,16 +60,33 @@ public class AnalyzerScheduler extends SimpleScheduler {
         System.out.println("********* Writing to: " + "/home/enrico/out.txt");
         BufferedWriter out = new BufferedWriter(fstream);
 
-        out.write("t_task: " + fulltime + "\n");
         for (Map.Entry<String, List<Long>> times : mExecutionTimes.entrySet()) {
-          out.write(times.getKey() + "\n");
-          out.write("jobs:   " + times.getValue().size() + "\n");
-          long avg = 0;
+          TestData data = new TestData();
+          data.id = times.getKey();
+          data.jobs = times.getValue().size();
+          data.params = mProcessedParameters.get(times.getKey());
+          long calctime = 0;
           for (long time : times.getValue()) {
-            avg += time;
+            calctime += time;
           }
-          avg /= times.getValue().size();
-          out.write("t_avg:  " + avg + "\n");
+          data.tCalc = calctime;
+          data.tParamAvg = calctime / data.params;
+          data.tJobAvg = calctime / data.jobs;
+          mData.add(data);
+        }
+
+        //
+        out.write("ID         jobs     params    tCalc   tJobAvg   tParamAvg\n");
+        System.out.println("mData size" + mData.size());
+        for (TestData data : mData) {
+          out.write(String.format(
+                  "%s\t%d\t%d\t%d\t%d\t%d\n",
+                  data.id.substring(0, 9),
+                  data.jobs,
+                  data.params,
+                  data.tCalc,
+                  data.tJobAvg,
+                  data.tParamAvg));
         }
         //Close the output stream
         out.close();
@@ -71,9 +94,6 @@ public class AnalyzerScheduler extends SimpleScheduler {
       catch (Exception e) {//Catch exception if any
         System.err.println("Error: " + e.getMessage());
       }
-
-
-
     }
     return done;
   }
@@ -82,5 +102,15 @@ public class AnalyzerScheduler extends SimpleScheduler {
   public void abort() {
     super.abort();
     System.out.println("*** ABORT!!!");
+  }
+
+  private class TestData {
+
+    public String id;
+    public int jobs;
+    public int params;
+    public long tCalc;
+    public long tParamAvg;
+    public long tJobAvg;
   }
 }
